027724e5be98422b284b544d2697fb62d1248fe7
YashwantTelang-1525 <yeshwantstelang@gmail.com>
2023-06-20 11:40:06 +0530

    loan service added

---
+++ loanplan/src/main/java/com/microservices/loanplan/LoanplanApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.loanplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanplanApplication.class, args);
+	}
+
+}
---
+++ loanplan/src/main/java/com/microservices/loanplan/controllers/UserController.java
@@ -0,0 +1,21 @@
+package com.microservices.loanplan.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @GetMapping("/public")
+    public String publicEndpoint() {
+        return "This is a public endpoint";
+    }
+
+    @GetMapping("/secure")
+    public String secureEndpoint() {
+        return "This is a secure endpoint";
+    }
+
+}
